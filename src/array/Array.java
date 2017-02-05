30be7167b3001a70467ae9f4394f443c963e5443
rizaalvin <rizaalvinazizi@gmail.com>
2017-02-05 19:10:23 +0700

    Array

---
+++ src/array/Array.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array;
+
+/**
+ *
+ * @author riani
+ */
+public class Array {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int [] jumlahhari;
+        jumlahhari = new int [7];
+        jumlahhari [0] = 31;
+        jumlahhari [1] = 28;
+        jumlahhari [2] = 31;
+        jumlahhari [3] = 30;
+        jumlahhari [4] = 31;
+        jumlahhari [5] = 30;
+        jumlahhari [6] = 31;
+        
+        System.out.println("Bulan Januari memiliki " + jumlahhari [2] + " hari. ");
+    }
+    
+}
